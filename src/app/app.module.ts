1b8440ba09216902ed51b5277db32e6b3eca5b6e
amisis97 <amisis97@gmail.com>
2020-02-16 20:21:20 +0100

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Angular auth
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCJ8cYiOz66eKr5nX_4ViS6qGNeLlRGj5Y',
+    authDomain: 'raktar-f8e14.firebaseapp.com',
+    databaseURL: 'https://raktar-f8e14.firebaseio.com',
+    projectId: 'raktar-f8e14',
+    storageBucket: 'raktar-f8e14.appspot.com',
+    messagingSenderId: '915921638794',
+    appId: '1:915921638794:web:e704146259209e9e3fa473',
+    measurementId: 'G-0PQ13DY3SC'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
