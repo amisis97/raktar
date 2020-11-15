532784785a017af0de0323add4e010c2383ee84d
amisis97 <amisis97@gmail.com>
2020-11-15 18:19:21 +0100

    github page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCJ8cYiOz66eKr5nX_4ViS6qGNeLlRGj5Y',
+    authDomain: 'raktar-f8e14.firebaseapp.com',
+    databaseURL: 'https://raktar-f8e14.firebaseio.com',
+    projectId: 'raktar-f8e14',
+    storageBucket: 'raktar-f8e14.appspot.com',
+    messagingSenderId: '915921638794',
+    appId: '1:915921638794:web:e704146259209e9e3fa473',
+    measurementId: 'G-0PQ13DY3SC'
+  }
 };
